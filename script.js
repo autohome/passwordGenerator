bd0dbe880ccba772c03f5f3e48d0ec7c7121fa13
JoshuaD <joshdadbin@Josh.local>
2023-03-09 20:11:37 -0800

    add all strings to pool

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 // Assignment code here
 
+// empty pool array
+var pool = []
+
+// alpha arrays
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const upperCaseArray = alpha.map((x) => String.fromCharCode(x));
+const lowerCaseArray = upperCaseArray.map(upperCaseArray => upperCaseArray.toLowerCase())
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// special char array
+const string = '!#$%&()*+,-./:;<=>?@[\]^_{|}~'
+const specialChar = [...string]
+
+
+
+
+console.log (upperCaseArray)
+console.log (lowerCaseArray)
+console.log (numbers)
+console.log (specialChar)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+var writePassword = function() {
+  // first prompt
+  var charPrompt = window.prompt ("Choose a length of 8 to 128 characters");
+    // error 
+    if (charPrompt > 128 || charPrompt < 8) {
+    writePassword();
+    }
+
+  // second Prompt
+
+  var lowerCase = window.confirm ('Would you like lowercase characters?')
+  // if yes
+  // if (lowerCase) { 
+  //   var combined = 
+
+  // }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+
+
+
 
   passwordText.value = password;
 
