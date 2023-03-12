296e8929c43be87c73f1a242804d558bafa06e20
JoshuaD <joshdadbin@Josh.local>
2023-03-11 22:02:25 -0800

    code refactored, all complete

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Assignment code here
 
-// empty pool array
+// global empty pool array
 var pool = [];
+
+// global count variable
 var count;
+
 // alpha arrays
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const upperCaseArray = alpha.map((x) => String.fromCharCode(x));
@@ -15,98 +18,77 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const string = '!#$%&()*+,-./:;<=>?@[\]^_{|}~'
 const specialCharArray = [...string]
 
-
-
-
-// console.log (upperCaseArray)
-// console.log (lowerCaseArray)
-// console.log (numbers)
-// console.log (specialChar)
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-var writePassword = function() {
-  // first prompt
+
+// first prompt
+function writePassword() {
   count = window.prompt ("Choose a length of 8 to 128 characters");
-    // error 
+    // error prompts
     if (count === null) {
       return;
     } else if (count > 128 || count < 8) {
-      window.alert ("ERROR: Please enter a numeric character between 8 and 128")
-        writePassword();
+      window.alert ("ERROR: Please enter a numeric character between 8 and 128");
+      writePassword();
     } else if (count === "") {
-      window.alert ("ERROR: Please enter a numeric character between 8 and 128")
-        writePassword();
+      window.alert ("ERROR: Please enter a numeric character between 8 and 128");
+      writePassword();
+    // if correct
     } else (
       lowerCase ()
     )
-    }
-
- // second Prompt
-var lowerCase = function() {
-  var x = window.confirm ('Would you like to include lowercase characters?')
-  
-  if (x) { 
-    pool.push(lowerCaseArray);
-    console.log("ifthisisilowercasetrue",pool);
-    upperCase()
-  } else upperCase();
 }
 
+// second Prompt
+function lowerCase() {
+  var x = window.confirm('Would you like to include lowercase characters?');
+    if (x) {
+      pool.push(...lowerCaseArray);
+      upperCase();
+    } else
+      upperCase();
+}
 
-
-  // Third prompt
-
-  
-var upperCase = function() { 
-  var x = window.confirm ('Would you like to include UPPERCASE characters?')
+// Third prompt
+function upperCase() { 
+  var x = window.confirm ('Would you like to include UPPERCASE characters?');
     if (x) {
       pool.push(...upperCaseArray);
       specialChar();
     } else 
-      specialChar()
+      specialChar();
 }
 
-  // F  ourth Prompt
+  // Fourth Prompt
 
-var specialChar = function () {
-  var x = window.confirm ('Would you like to include Special characters?')
-  
-  if (x) { 
-    pool.push(...specialCharArray);
+function specialChar () {
+  var x = window.confirm ('Would you like to include Special characters?')  ;
+    if (x) { 
+      pool.push(...specialCharArray);
       generatePassword();
-  } else if (!x && pool === null){
-    window.alert ('ERROR: Please choose at least one set of characters.')
-  } else (
-    generatePassword()
+    } else if (!x && pool === null){
+      window.alert ('ERROR: Please choose at least one set of characters.');
+    } else (
+      generatePassword();
     )
-  }
+}
 
-
-var generatePassword = function () {
-
+// generate random string from pool
+function generatePassword() {
   var password = '';
     for (var i = 0; i < count; i++) {
         password += pool[Math.floor(Math.random() * pool.length)]
-      }
-      return password
     }
+    // Write password to the #password input
+    displayPassword(password)    
+}
 
-      
-
-  function writePassword() {
-    var password = generatePassword();
+  function displayPassword(password) {
     var passwordText = document.querySelector("#password");
-  
     passwordText.value = password;
-  
-  }
-  
-
-
+}
 
 
 // // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
